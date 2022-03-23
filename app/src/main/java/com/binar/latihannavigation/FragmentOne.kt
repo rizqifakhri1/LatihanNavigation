3fd1e4d1966ce3f6f7a0c6be6c979e9cf81ef223
rizqifakhri1 <rizqifakhri@gmail.com>
2022-03-23 16:04:53 +0700

    Add code to send data from fragmentOne.kt

--- app/src/main/java/com/binar/latihannavigation/FragmentOne.kt
+++ app/src/main/java/com/binar/latihannavigation/FragmentOne.kt
@@ -5,13 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.binar.latihannavigation.databinding.FragmentOneBinding
 
 class FragmentOne : Fragment() {
+
+    private var _binding : FragmentOneBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        val EXTRA_NAME = "EXTRA_NAME"
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        _binding = FragmentOneBinding.inflate(inflater, container, false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnFragmentKedua.setOnClickListener{
+            val mBundle = Bundle ()
+            mBundle.putString(EXTRA_NAME, "Binarian Pertama")
+            it.findNavController().navigate(R.id.action_fragmentOne_to_fragmentTwo, mBundle)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
